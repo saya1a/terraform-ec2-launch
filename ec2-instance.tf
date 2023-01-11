58e625d3b901590a85a279db38d5516af85146a5
saya1a <40052644+saya1a@users.noreply.github.com>
2023-01-11 22:23:11 +0530

    added keypair for ssh login

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,12 +1,20 @@
 resource "aws_instance" "test1" {
-  count	= var.ec2_count
+  count = var.ec2_count
   ami = var.ami_id
   instance_type = var.instance_type
-  security_groups = [aws_security_group.TF_SG.name]
-
-
+  key_name = "tf-key"
+# Security group association
+  security_groups = [aws_security_group.TF-SG.name]
+  
   tags = {
     Name = "test-instance"
   }
+  
+}
+
+# Generate kaypair from ssh-keygen -t rsa -b 2048 and paste at public_key
+resource "aws_key_pair" "TF_key" {
+  key_name = "tf-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQkjkhhghjvH5Hq1qi9odFye8HEA1pSPi8YGaBJgkdS7XQpStD9xN8XtMi3Zmch0c+F/Z58pYswuGKB8DoccX3Kcr5ouxkMZnWiOiCaIpAtT7mF0HxtQ== root@master-node"
   
 }
