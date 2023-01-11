28b143506c47ad7e0604db09c12b240e9210adab
saya1a <40052644+saya1a@users.noreply.github.com>
2023-01-11 19:32:01 +0530

    added security group for the instance

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "test1" {
   count	= var.ec2_count
   ami = var.ami_id
   instance_type = var.instance_type
+  security_groups = [aws_security_group.TF_SG.name]
 
 
   tags = {
