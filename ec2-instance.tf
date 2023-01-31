091368ef3e810c278e5df3e00918a198f5f5733d
saya1a <40052644+saya1a@users.noreply.github.com>
2023-01-31 23:22:00 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,10 +1,16 @@
+provider "aws" {
+region = "us-east-1"
+}
 resource "aws_instance" "test1" {
   count = var.ec2_count
   ami = var.ami_id
-  instance_type = var.instance_type
-  key_name = "tf-key"
+#  instance_type = var.instance_type
+  instance_type = var.instance_type[count.index]
+
+
+#  key_name = "tf-key"
 # Security group association
-  security_groups = [aws_security_group.TF-SG.name]
+#  security_groups = [aws_security_group.TF-SG.name]
   
   tags = {
     Name = "test-instance"
@@ -13,8 +19,18 @@ resource "aws_instance" "test1" {
 }
 
 # Generate kaypair from ssh-keygen -t rsa -b 2048 and paste at public_key
+
 resource "aws_key_pair" "TF_key" {
-  key_name = "tf-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQkjkhhghjvH5Hq1qi9odFye8HEA1pSPi8YGaBJgkdS7XQpStD9xN8XtMi3Zmch0c+F/Z58pYswuGKB8DoccX3Kcr5ouxkMZnWiOiCaIpAtT7mF0HxtQ== root@master-node"
-  
+  key_name   = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "TF-key" {
+    content  = tls_private_key.rsa.private_key_pem
+    filename = "TF-private-key"
 }
